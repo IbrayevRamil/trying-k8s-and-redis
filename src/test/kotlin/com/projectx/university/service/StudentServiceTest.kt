f2db495e7af38928f85ba5325891318bd35d7a48
ramil.ibrayev <ramil1dan@gmail.com>
2020-08-17 21:55:27 +0300

    added junit 5 and mockk added StudentServiceTest

---
+++ src/test/kotlin/com/projectx/university/service/StudentServiceTest.kt
@@ -0,0 +1,57 @@
+package com.projectx.university.service
+
+import com.projectx.university.dto.CreationOutput
+import com.projectx.university.dto.Student
+import com.projectx.university.repository.StudentRepository
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+
+internal class StudentServiceTest {
+
+    private val studentRepositoryMock: StudentRepository = mockk()
+    private val sut: StudentService = StudentService(studentRepositoryMock)
+
+    @BeforeEach
+    fun init() {
+        clearAllMocks()
+    }
+
+    @Test
+    fun addStudent() {
+        // given
+        val student = Student("testId", "testName")
+        every { studentRepositoryMock.addStudent(student) } returns Mono.just(true)
+
+        // when
+        val result = sut.addStudent(student)
+
+        // then
+        StepVerifier.create(result)
+                .expectNext(CreationOutput(true))
+                .verifyComplete()
+    }
+
+    @Test
+    fun getStudent() {
+        // given
+        val studentId = "testId"
+        val expectedStudent = Student(studentId, "testName")
+        every { studentRepositoryMock.getStudent(studentId) } returns Mono.just(expectedStudent)
+
+        // when
+        val result = sut.getStudent(studentId)
+
+        // then
+        StepVerifier.create(result)
+                .expectNext(expectedStudent)
+                .verifyComplete()
+    }
+}
